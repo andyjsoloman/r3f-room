8f10364446705a2b0eab93414d3bb25b50a2c15e
Andy Soloman <andyjsoloman@gmail.com>
2024-01-09 22:19:15 -0800

    add movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
+import { KeyboardControls } from "@react-three/drei";
 
 function App() {
   return (
-    <Canvas>
-      <Experience />
-    </Canvas>
+    <KeyboardControls
+      map={[
+        { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        { name: "backward", keys: ["ArrowDown", "KeyS"] },
+        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+        { name: "jump", keys: ["Space"] },
+      ]}
+    >
+      <Canvas>
+        <Experience />
+      </Canvas>
+    </KeyboardControls>
   );
 }
 
--- src/Self.jsx
+++ src/Self.jsx
@@ -1,9 +1,51 @@
 import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
+import { useRef, useEffect } from "react";
 
 export default function Self() {
+  const body = useRef();
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+
+  const jump = () => {
+    body.current.applyImpulse({ x: 0, y: 40, z: 0 });
+  };
+
+  useEffect(() => {
+    subscribeKeys(
+      (state) => state.jump,
+      (value) => {
+        if (value) jump();
+      }
+    );
+  }, []);
+
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 100 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+    }
+    if (backward) {
+      impulse.z += impulseStrength;
+    }
+    if (leftward) {
+      impulse.x -= impulseStrength;
+    }
+    if (rightward) {
+      impulse.x += impulseStrength;
+    }
+
+    body.current.applyImpulse(impulse);
+  });
+
   return (
     <>
-      <RigidBody canSleep={false}>
+      <RigidBody ref={body} canSleep={false}>
         <mesh castShadow>
           <sphereGeometry args={[1, 32, 16]} />
           <meshStandardMaterial color="coral" />
